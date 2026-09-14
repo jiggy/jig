9286995af79b282153b123dcd84ad5bb5675a991
zvictor <zvictor@users.noreply.github.com>
2026-09-14 21:51:26 +0200

    formatting: expand blocks in all `jig.ts` files

--- examples/request-triage/jig.ts
+++ examples/request-triage/jig.ts
@@ -1,3 +1,6 @@
 import { defineJig, discover } from '@jigging/jig'
 
-export default defineJig({ flows: discover('flows'), bindings: discover('bindings') })
+export default defineJig({
+  flows: discover('flows'),
+  bindings: discover('bindings'),
+})
